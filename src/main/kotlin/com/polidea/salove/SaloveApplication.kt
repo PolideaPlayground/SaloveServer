ffd7570430c0b6b56c96fbf534ee0fcb01d1a08c
Łukasz Gajowy <lukasz.gajowy@polidea.com>
2017-08-25 16:37:44 +0200

    Spring initializr generated project

---
+++ src/main/kotlin/com/polidea/salove/SaloveApplication.kt
@@ -0,0 +1,11 @@
+package com.polidea.salove
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SaloveApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SaloveApplication::class.java, *args)
+}
