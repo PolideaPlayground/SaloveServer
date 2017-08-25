205859f4c16b952b920c01d7f0586a1bf4a12e94
Łukasz Gajowy <lukasz.gajowy@polidea.com>
2017-08-25 19:16:44 +0200

    Room reservation works!

--- src/main/kotlin/com/polidea/salove/domain/reservation/Reservation.kt
+++ src/main/kotlin/com/polidea/salove/domain/reservation/Reservation.kt
@@ -2,15 +2,55 @@ package com.polidea.salove.domain.reservation
 
 import com.google.api.client.util.DateTime
 import com.google.api.services.calendar.Calendar
+import com.google.api.services.calendar.model.Event
+import com.google.api.services.calendar.model.EventDateTime
 import org.springframework.stereotype.Service
+import java.util.*
+
 
 @Service
 class Reservation(val calendar: Calendar) {
 
-    fun create(id: Int, eventStart: Long, eventEnd: Long) {
+    val CALENDAR_ID: String = "primary"
 
+    fun create(roomId: Int) {
+        val event = Event()
+        event.summary = "Room $roomId reservation!"
+
+        val now: Date = Date()
+        setStartDate(event, now)
+        setEndTime(event, now)
+        calendar.events().insert(CALENDAR_ID, event).execute()
+
+        read()
+    }
+
+    private fun setEndTime(event: Event, now: Date) {
+        event.end = EventDateTime()
+        event.end.dateTime = DateTime(getNextFullHour(now))
+        event.end.timeZone = "Europe/Warsaw"
+    }
+
+    private fun setStartDate(event: Event, now: Date) {
+        event.start = EventDateTime()
+        event.start.dateTime = DateTime(now)
+        event.start.timeZone = "Europe/Warsaw"
+    }
+
+    private fun getNextFullHour(now: Date): Date? {
+        val cal = java.util.Calendar.getInstance()
+        cal.time = now
+        cal.add(java.util.Calendar.HOUR_OF_DAY, 1) // adds one hour
+        cal.set(java.util.Calendar.MINUTE, 0)
+        cal.set(java.util.Calendar.SECOND, 0)
+        val nextFullHour = cal.getTime()
+        return nextFullHour
+    }
+
+    fun read() {
         val now = DateTime(System.currentTimeMillis())
-        val events = calendar.events().list("primary").setMaxResults(10).setTimeMin(now)
+
+        val events = calendar.events().list(CALENDAR_ID).setMaxResults(10).setTimeMin(now)
                 .setOrderBy("startTime").setSingleEvents(true).execute()
         val items = events.getItems()
         if (items.size == 0) {
--- src/main/kotlin/com/polidea/salove/web/rest/ReservationsController.kt
+++ src/main/kotlin/com/polidea/salove/web/rest/ReservationsController.kt
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
-data class ReservationRequest(var id: Int, var start: Long, var end: Long)
+data class ReservationRequest(var id: Int)
 
 @RestController
 @RequestMapping("rooms")
 class ReservationsController(val reservation: Reservation) {
 
-    data class ReservationRequest(var id: Int, var start: Long, var end: Long)
+    data class ReservationRequest(var roomId: Int)
     
     @PostMapping("/{reservation}")
     fun findByLastName(@RequestBody body: ReservationRequest)
-            = reservation.create(body.id, body.start, body.end)
+            = reservation.create(body.roomId)
 }
 
 
